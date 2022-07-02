561fde42b2ef916c53dec3cd4c4795a8799c841c
adri-vljkvc <adrijana.veljkovic@o2mail.de>
2022-07-02 17:33:34 +0200

    Aufgabe 11 (Versuch)

---
+++ Steckbrief/Aufgabe_11/moveable.ts
@@ -0,0 +1,27 @@
+namespace L11 {
+    
+export abstract class Moveable {
+    posX: number;
+    posY: number;
+
+    velocityX: number;
+    velocityY: number;
+
+    mor: number
+
+    constructor(_position: Vector, _velocity: Vector, _mor:number) {
+          this.posX = _position.x; 
+          this.posY = _position.y; 
+
+          this.velocityX = _velocity.x;
+          this.velocityY = _velocity.y;
+
+          this.mor = _mor;
+    }
+
+    abstract draw();
+    abstract update();
+    
+    abstract click?(_x: number, _y: number);
+}
+}
